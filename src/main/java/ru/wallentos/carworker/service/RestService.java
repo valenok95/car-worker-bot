bbfc64dd0c4c9a4f909ae06de7b0a0879e1a259b
valentin.zhuravel <valentin.zhuravel@x5.ru>
2023-06-16 12:22:58 +0300

    добавил курс USD/KRW по naver

--- src/main/java/ru/wallentos/carworker/service/RestService.java
+++ src/main/java/ru/wallentos/carworker/service/RestService.java
@@ -1,8 +1,10 @@
 package ru.wallentos.carworker.service;
 
+import java.io.IOException;
 import java.util.Map;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 public class RestService {
     @Value("${ru.wallentos.carworker.exchange-api.host-cbr}")
     private String cbrMethod;
+    @Value("${ru.wallentos.carworker.exchange-api.host-naver}")
+    private String naverMethod;
     private RestTemplate restTemplate;
 
     private UtilService utilService;
@@ -32,7 +36,16 @@ public class RestService {
                 = restTemplate.getForEntity(cbrMethod, String.class);
         conversionRatesMap = utilService.backRatesToConversionRatesMap(response.getBody());
         log.info("курс обновлён {}", conversionRatesMap);
-        cbrUsdKrwMinus20 = (conversionRatesMap.get("KRW") / conversionRatesMap.get("USD")) - 20;
+        cbrUsdKrwMinus20 = Double.parseDouble(getNaverRate()) - 20;
+    }
+
+    public String getNaverRate() {
+        try {
+            var document = Jsoup.connect(naverMethod).get();
+            return document.getElementById("select_from").childNodes().get(3).attributes().asList().get(0).getValue();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
